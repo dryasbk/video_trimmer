14d4b27a1c0cb95891cf5fb01f8830687bb2b7c4
Dr. Yasser Albrikeet <dryasbk@gmail.com>
2024-09-19 19:19:34 +0300

    update key and context

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -6,7 +6,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +22,7 @@ class HomePage extends StatelessWidget {
               type: FileType.video,
               allowCompression: false,
             );
-            if (result != null) {
+            if (result != null && context.mounted) {
               File file = File(result.files.single.path!);
               Navigator.of(context).push(
                 MaterialPageRoute(builder: (context) {
