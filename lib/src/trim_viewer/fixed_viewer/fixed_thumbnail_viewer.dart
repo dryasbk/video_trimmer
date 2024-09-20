6000a20fa07579a21d00cb8f5b273e1f1667d139
Dr. Yasser Albrikeet <dryasbk@gmail.com>
2024-09-20 13:32:09 +0300

    replace video thumpnail and upgrade android to 34

--- lib/src/trim_viewer/fixed_viewer/fixed_thumbnail_viewer.dart
+++ lib/src/trim_viewer/fixed_viewer/fixed_thumbnail_viewer.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:tmp_path/tmp_path.dart';
+import 'package:path/path.dart' as p;
 import 'package:transparent_image/transparent_image.dart';
-import 'package:video_thumbnail/video_thumbnail.dart';
+// import 'package:video_thumbnail/video_thumbnail.dart';
+import 'package:fc_native_video_thumbnail/fc_native_video_thumbnail.dart';
 
 class FixedThumbnailViewer extends StatelessWidget {
   final File videoFile;
@@ -31,17 +34,58 @@ class FixedThumbnailViewer extends StatelessWidget {
     final String videoPath = videoFile.path;
     double eachPart = videoDuration / numberOfThumbnails;
     List<Uint8List?> byteList = [];
+
+// Future<void> run() async {
+//       try {
+//         var plugin = FcNativeVideoThumbnail();
+//         final destFile = tmpPath() + p.extension(videoPath);
+//         await plugin.getVideoThumbnail(
+//             srcFile: videoPath,
+//             destFile: destFile,
+//             width: 50 * 8,
+//             height: 50 ,
+//             quality: quality,
+//             // srcFileUri: isSrcUri,
+//             format: 'jpeg');
+//         if (await File(destFile).exists()) {
+//           var imageFile = File(destFile);
+//           // var decodedImage =
+//           //     await decodeImageFromList(imageFile.readAsBytesSync());
+//         //  final destImgSize =
+//         //       'Decoded size: ${decodedImage.width}x${decodedImage.height}';
+
+//         // } else {
+//         //   error = 'No thumbnail extracted';
+//         }
+//       } catch (err) {
+//         print( err.toString());
+//       }
+//     }
+
+// //
     // the cache of last thumbnail
     Uint8List? lastBytes;
     for (int i = 1; i <= numberOfThumbnails; i++) {
       Uint8List? bytes;
       try {
-        bytes = await VideoThumbnail.thumbnailData(
-          video: videoPath,
-          imageFormat: ImageFormat.JPEG,
-          timeMs: (eachPart * i).toInt(),
-          quality: quality,
-        );
+        var plugin = FcNativeVideoThumbnail();
+        final destFile = tmpPath() + p.extension(videoPath);
+        await plugin.getVideoThumbnail(
+            srcFile: videoPath,
+            destFile: destFile,
+            width: 50 * 8,
+            height: 50,
+            quality: quality,
+            // srcFileUri: isSrcUri,
+            format: 'jpeg');
+        if (await File(destFile).exists()) {
+          var imageFile = File(destFile);
+          bytes = imageFile.readAsBytesSync();
+          //   var decodedImage =
+          //       await decodeImageFromList(imageFile.readAsBytesSync());
+          //  final destImgSize =
+          //       'Decoded size: ${decodedImage.width}x${decodedImage.height}';
+        }
       } catch (e) {
         debugPrint('ERROR: Couldn\'t generate thumbnails: $e');
       }
